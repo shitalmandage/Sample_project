c289daeb257e8e97a58c217dfeb98c905944624f
shitalmandage <shitalmandage14@gmail.com>
2018-03-26 12:01:37 +0530

    Add files via upload

---
+++ Algo/split_string.py
@@ -0,0 +1,31 @@
+import sys
+
+string=input()
+w=int(input())
+j=0
+splited_string=string.split(" ")
+print(splited_string)
+i=0
+lst=[]
+while(1):
+    l=0
+    cnt=0
+    s=""
+    while(l<w):
+        l=len(splited_string[i])+1
+        cnt+=1
+    i+=1
+    
+
+
+
+while(i!=len(string)):
+    i+=w
+    if string[i]==' ':
+        lst.append(string[j:i])
+    else:
+
+
+
+    
+
